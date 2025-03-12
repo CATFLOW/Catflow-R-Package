f3c4f67604abc564d2f59ba02f216472f5ede906
Wienhöfer <oh3360@kit.edu>
2025-03-12 15:28:22 +0100

    Update plot.catf.bal.R

--- Catflow/R/plot.catf.bal.R
+++ Catflow/R/plot.catf.bal.R
@@ -18,8 +18,8 @@ function(bilanz, ylim, unit = "m³", on.top = FALSE, stay = FALSE, ...)
  Ofa <- bilanz[, 12]
  Nied <- bilanz[, 14]
  Nied_mm <- bilanz[, 15]
-  Interzep <- bilanz[, 16]
-BodenVerd <- bilanz[, 17]
+ Interzep <- bilanz[, 16]
+ BodenVerd <- bilanz[, 17]
  Transp <- bilanz[, 18]
 
 #.. Calculations
@@ -30,14 +30,14 @@ BodenVerd <- bilanz[, 17]
   # Totale <- -Nied + Auffeuchtung  - (Senken +Unterrand +Rechtrand + Linkrand) +Ofa +Interzep 
   # --> equals surfbal + Internbil
 
- area <-  Nied/(Nied_mm/1000)                     # calculate Surface area from Precipitation data
+ # calculate Surface area from Precipitation data 
+ area <-  Nied/(Nied_mm/1000)                     
 
  if(unit =="mm")
   {  unitfac <- 1000/area[length(Nied)]
  } else unitfac <- 1
 
   
-
  #.. Plotting
 
  if(missing(ylim)) ylim = range(c(Nied, Auffeuchtung, Ofa, Interzep, BodenVerd, surfbal, Internbil), 
@@ -45,13 +45,13 @@ BodenVerd <- bilanz[, 17]
 
   opa <- par(oma = c(0,0,0,9.5) )
   
-  plot(Time/86400, Nied*unitfac, typ = "l", , ylab = unit, xlab = "Time [d]", ylim = ylim, ...)
+ plot(Time/86400, Nied*unitfac, typ = "l", , ylab = unit, xlab = "Time [d]", ylim = ylim, ...)
  lines(Time/86400, Ofa*unitfac,  col = "blue")
  lines(Time/86400, -Rechtrand*unitfac,  col = "darkblue", lty = 2)
  lines(Time/86400, -Unterrand*unitfac,  col = "brown")
- # lines(Time/86400, -Linkrand*unitfac, col = "grey" , lty = 2)
+  # lines(Time/86400, -Linkrand*unitfac, col = "grey" , lty = 2)
  lines(Time/86400, Auffeuchtung*unitfac,  col = "lightblue")
- lines(Time/86400, BodenVerd*unitfac,  col = "green")
+ lines(Time/86400, Interzep*unitfac,  col = "green")
  lines(Time/86400, -Senken*unitfac, col = "darkgreen" , lty = 3)
  
  lines(Time/86400, surfbal*unitfac,  col = "red", lty = 2)
@@ -64,7 +64,7 @@ BodenVerd <- bilanz[, 17]
   
  legend("right", c("Precipitation", "Surface runoff", "Right bound. flux",
         "Left bound. flux",
-         "Lower bound. flux", "Soil moist.", "Soil evap.", "Transpiration", 
+         "Lower bound. flux", "Soil moist.", "Interception", "Evapotranspiration", 
          "Surface bal.","Internal bal."),
    title ="Legend",
    col = c(1, "blue", "darkblue", "grey", "brown", "lightblue", "green", "darkgreen", "red", 6), 
@@ -78,7 +78,3 @@ BodenVerd <- bilanz[, 17]
 return(invisible(data.frame(time = Time, "Surf.bal" = surfbal, "Totale" =  surfbal + Internbil) ) ) 
 }
  
-
-
-
- ### JW Umrechnung in mm!
