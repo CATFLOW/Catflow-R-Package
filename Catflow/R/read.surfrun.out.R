1336ea3922ae0c0c3ff702b8504f50a931855cc3
Wienhöfer <oh3360@kit.edu>
2025-03-14 20:30:40 +0100

    update read.surfrun
    reads both 5 and 6 column output

--- Catflow/R/read.surfrun.out.R
+++ Catflow/R/read.surfrun.out.R
@@ -1,48 +1,45 @@
-read.surfrun.out <-
-function(filename) {
+read.surfrun.out <-           # JW  check output across versions, check conversion
+function(filename,
+         GMT.off = -3600, 
+         timzon = "GMT", 
+         plotting = TRUE)
+  {
   start.date <- read.table(filename, nrows = 1)  # currently unused
   
   hdr <- read.table(filename, nrows = 1, skip = 1)
   
   qo <- read.table(filename, skip = 2)
-  names(qo) <- c("ihg", "time", "time+dt","Neff", "q", "cum.q")
-  qo <- qo[, c("time", "time+dt","Neff", "q", "cum.q","ihg" )]
-  
-  qconv <- vector("numeric", nrow(qo))       
-  for( i in 1:ncol(qo) ) qconv <- cbind(qconv, hdr[[i]] * qo[,i] )
-  
-  if(nrow(qo) > 1 ) { qconv <- data.frame(qconv[,-1])
-   } else    qconv <- qconv[,-1]
-  
-  names(qconv) <- names(qo)
-  
-  print(paste(filename, "read sucessfully."))
+ 
+  # old version: 5 columns, new version: six columns
+ 
+ if(ncol(qo) == 6)
+ { names(qo) <- c("ihg", "time", "time+dt","Neff", "q", "cum.q")
+   qo <- qo[, c("time", "time+dt","Neff", "q", "cum.q","ihg" )]
+  } else {
+   names(qo) <- c("ihg", "time", "Neff", "q", "cum.q")
+   qo <- qo[, c("time", "Neff", "q", "cum.q","ihg" )]
+  }
+
+  # convert with given factors: all columns but last (slope ID)
+  qconv <- qo 
+  for( i in 1:(ncol(qo)-1) ) qconv[,i] <- hdr[[i]] * qo[,i] 
+
+
+ ## convert to zoo
+ timevec <- as.POSIXct(strptime( paste( start.date[1], start.date[2]), "%d.%m.%Y %H:%M:%S"), 
+                       tz = timzon) +  GMT.off  + qo$time 
+ 
+ qo <- zoo(qo, timevec)
+  qconv <- zoo(qconv, timevec)
+                       
+ # Plotting (optional)
+  if(plotting){
+     plot(qo[,c("q","cum.q","Neff")], type = "s", 
+         main = strsplit(filename, "/")[[1]][
+           length(strsplit(filename, "/")[[1]])]) 
+     }
+ 
   
   return(list("orig" = qo, "converted" = qconv)) 
-
-# read.surfrun.old <-
-#  function(filename) {
-#   
-# start.date <- read.table(filename, nrow = 1)  # currently unused
-# 
-# hdr <- read.table(filename, nrow = 1, skip = 1)
-# 
-# qo <- read.table(filename, skip = 2)
-#  names(qo) <- c("ihg", "time", "Neff", "q", "cum.q")
-#  qo <- qo[, c("time", "Neff", "q", "cum.q","ihg" )]
-#  
-# qconv <- vector("numeric", nrow(qo))       
-# for( i in 1:ncol(qo) ) qconv <- cbind(qconv, hdr[[i]] * qo[,i] )
-#  
-#  if(nrow(qo) > 1 ) { qconv <- data.frame(qconv[,-1])
-#  } else    qconv <- qconv[,-1]
-#  
-#  names(qconv) <- names(qo)
-# 
-# print(paste(filename, "read sucessfully."))
-# 
-# return(list("orig" = qo, "converted" = qconv))
-#  }
-
 }
 
